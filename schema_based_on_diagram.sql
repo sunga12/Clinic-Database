54741896cf46a15b8bd79eb733e8c4addcabb50d
rabayarabu <rabu9149@gmail.com>
2023-07-26 22:23:19 +0600

    Add foreign key and create index

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -33,3 +33,17 @@ CREATE TABLE invoice_items(
 	invoice_id INT REFERENCES invoices(id),
 	treatment_id INT REFERENCES treatments(id)
 );
+
+ALTER TABLE medical_histories ADD CONSTRAINT fk_patients FOREIGN KEY (patient_id) REFERENCES patients(id);
+ALTER TABLE invoices ADD CONSTRAINT fk_medical_histories FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id);
+ALTER TABLE invoice_items ADD CONSTRAINT fk_treatments FOREIGN KEY (treatment_id) REFERENCES treatments(id);
+ALTER TABLE invoice_items ADD CONSTRAINT fk_invoices FOREIGN KEY (invoice_id) REFERENCES invoices(id);
+ALTER TABLE treatments_histories ADD CONSTRAINT fk_treatments FOREIGN KEY (treatments_id) REFERENCES treatments(id);
+ALTER TABLE treatments_histories ADD CONSTRAINT fk_histories FOREIGN KEY (medical_histories_id) REFERENCES medical_histories(id);
+
+CREATE INDEX idx_patient_id ON medical_histories(patient_id);
+CREATE INDEX idx_medical_history_id ON invoices(medical_history_id);
+CREATE INDEX idx_invoice_id ON invoice_items(invoice_id);
+CREATE INDEX idx_treatment_id ON invoice_items(treatment_id);
+CREATE INDEX idx_medical_histories_id ON treatments_histories(medical_histories_id);
+CREATE INDEX idx_treatments_id ON treatments_histories(treatments_id);
